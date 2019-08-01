b691ff30a7c301e77def3ff60d8a339789b2a6e8
unknown <bolivar014@gmail.com>
2019-08-01 10:37:01 -0500

    Integrando vista Show Posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
     @section('content')
-        <h1>Mensaje de prueba.. vista Show</h1>
+        <h1 class="title h2 text-center"> ID: {{$post->id}} </h1>    
+           
+        <hr class="my-2">
 
+        <div class="card mb-3 mr-auto ml-auto" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                <img src="{{ $post->image }}" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text"><strong style="color: red;">Comentario: </strong> {{ $post->posts }}</p>
+                    <p class="card-text"><small class="text-muted">Creado el: {{ $post->created_at }}</small></p>
+                </div>
+                </div>
+            </div>
+        </div>
         
     @endsection
